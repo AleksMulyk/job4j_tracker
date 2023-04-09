9e2c05ce5b57d2af5c0d52b916f7fb7a0a7a18f3
AleksMulyk <mavpakuusa@gmail.com>
2023-04-09 17:14:33 -0500

    Правила Checkstyle [#504981 #408141]

--- src/main/java/checkstyle/Broken.java
+++ src/main/java/checkstyle/Broken.java
@@ -6,19 +6,19 @@ public class Broken {
     private int sizeOfEmpty = 10;
     private String name;
 
-    Broken() {
+    public Broken() {
     }
 
-    void echo() {
+    public void echo() {
 
     }
 
-    void media(Object obj) {
+    public void media(Object obj) {
         if (obj != null) {
             System.out.println(obj);
         }
     }
 
-    void method(int a, int b, int c, int d, int e, int f, int g, int h) {
+    public void method(int a, int b, int c, int d, int e, int f, int g, int h) {
     }
 }
