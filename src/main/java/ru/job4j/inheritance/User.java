df4f75834aabfced7851c0504af284c71220ff8b
AleksMulyk <mavpakuusa@gmail.com>
2023-04-15 21:35:35 -0500

    3. Вызов конструктора родителя super(...) [#6837]

---
+++ src/main/java/ru/job4j/inheritance/User.java
@@ -0,0 +1,9 @@
+package ru.job4j.inheritance;
+
+import ru.job4j.oop.Base;
+
+public class User extends Base {
+    public static void main(String[] args) {
+        User user = new User();
+    }
+}
