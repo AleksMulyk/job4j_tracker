0e8d1bfd954953c0ba2b533739627b745590a05c
AleksMulyk <mavpakuusa@gmail.com>
2023-04-23 20:13:33 -0500

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/DropArray.java
+++ src/main/java/ru/job4j/tracker/DropArray.java
@@ -17,5 +17,8 @@ public class DropArray {
         for (int index = 0; index < rsl.length; index++) {
             System.out.println(rsl[index]);
         }
+        String[] names1 = {"Petr", null, "Ivan", "Stepan", "Fedor"};
+        System.arraycopy(names1, 2, names1, 1, 3);
+        System.out.println(Arrays.toString(names1));
     }
 }
