2507895ab5e1698308b5bed0dab4bc893f67332b
AleksMulyk <mavpakuusa@gmail.com>
2023-04-29 20:42:32 -0500

    3.4. Реализация класса StartUI. Удаление заявки [#500747]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -39,6 +39,15 @@ public class StartUI {
                 } else {
                     System.out.println("Error replaced item.");
                 }
+            } else if (select == 3) {
+                System.out.println("=== Delete item ===");
+                System.out.print("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                if (tracker.delete(id)) {
+                    System.out.println("Item is deleted successful.");
+                } else {
+                    System.out.println("Error deleted item.");
+                }
             } else if (select == 6) {
                 run = false;
             }
